c0a824b2c1f911c350aacc27600328ef059a6d1d
vice garin <vicegpereda12@gmail.com>
2024-05-05 19:46:00 -0400

    datos basicos

--- hola muundo.py
+++ hola muundo.py
@@ -1,3 +1,78 @@
 #hello world
 
 print("hola mundo")
+
+# Definición de una variable entera
+numero = 10
+
+# Operaciones aritméticas
+suma = numero + 5
+resta = numero - 3
+multiplicacion = numero * 2
+division = numero / 3
+
+# Imprimir los resultados
+print("Número original:", numero)
+print("Suma:", suma)
+print("Resta:", resta)
+print("Multiplicación:", multiplicacion)
+print("División:", division)
+
+
+
+# Definir dos variables float
+numero1 = 3.14
+numero2 = 2.718
+
+# Realizar algunas operaciones con números float
+suma = numero1 + numero2
+resta = numero1 - numero2
+producto = numero1 * numero2
+division = numero1 / numero2
+
+# Imprimir los resultados
+print("Suma:", suma)
+print("Resta:", resta)
+print("Producto:", producto)
+print("División:", division)
+
+
+
+
+
+
+
+
+# Definir dos números complejos
+complejo1 = 3 + 2j
+complejo2 = 1 - 4j
+
+# Sumar los números complejos
+suma = complejo1 + complejo2
+print("Suma:", suma)
+
+# Restar los números complejos
+resta = complejo1 - complejo2
+print("Resta:", resta)
+
+# Multiplicar los números complejos
+multiplicacion = complejo1 * complejo2
+print("Multiplicación:", multiplicacion)
+
+# Dividir los números complejos
+division = complejo1 / complejo2
+print("División:", division)
+
+# Obtener la parte real e imaginaria de un número complejo
+parte_real = complejo1.real
+parte_imaginaria = complejo1.imag
+print("Parte Real:", parte_real)
+print("Parte Imaginaria:", parte_imaginaria)
+
+# Calcular el conjugado de un número complejo
+conjugado = complejo1.conjugate()
+print("Conjugado:", conjugado)
+
+# Calcular el módulo de un número complejo
+modulo = abs(complejo1)
+print("Módulo:", modulo)
