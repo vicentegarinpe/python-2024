6c9218f27c968e36ae8e9bf4f3b8e58e515b784c
vice garin <vicegpereda12@gmail.com>
2024-06-09 18:01:15 -0400

    ejercicio 3 de guia

--- #ejercicio 4.py
+++ #ejercicio 4.py
@@ -1,9 +1,9 @@
-#ejercicio3
+#ejercicio3 guia aprendizaje
 
 aves={"aguila","canario","pato"}
 animales_terrestres={"leon","elefante","nutria"}
 animales_acuaticos={"pato","delfin","nutria"}
-
+#print set
 print(f"aves: {aves}")
 print(f"animal esterrestres: {animales_terrestres}")
 print(f"animales acuaticos: {animales_acuaticos}")
@@ -12,6 +12,7 @@ aves.add("pelicano")
 animales_terrestres.add("lobo")
 animales_acuaticos.add("pez espada")
 
+#set actualizado
 print(f"aves: {aves}")
 print(f"animal esterrestres: {animales_terrestres}")
 print(f"animales acuaticos: {animales_acuaticos}")
@@ -20,6 +21,8 @@ intersectionaves=aves.intersection(animales_terrestres)
 interseccionterrestres=aves.intersection(animales_acuaticos)
 intersectionacuaticos=animales_terrestres.intersection(animales_acuaticos)
 
+
+print(f"set actualizado junto a las intersecciones:")
 print(f"aves y animales terrestres:{intersectionaves}")
 print(f"aves y acuaticos:{interseccionterrestres}")
 print(f"acuaticos y terrestres:{intersectionacuaticos}")
