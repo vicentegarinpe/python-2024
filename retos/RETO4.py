4360e9904294379ff8610cbfb9eaa10453be1165
vice garin <vicegpereda12@gmail.com>
2024-06-23 22:07:38 -0400

    reto 4

---
+++ retos/RETO4.py
@@ -0,0 +1,11 @@
+# reto 4
+
+numeros = list(range(10, 51, 2))
+
+# Sumar todos los números en la lista
+suma = sum(numeros)
+
+# Mostrar el resultado
+print(f"Los números en el rango 10 al 50 incrementando de 2 en 2 son: {numeros}")
+print(f"La suma de estos números es: {suma}")
+
